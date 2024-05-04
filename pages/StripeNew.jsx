e52ede0e396e1b66a400f85b444a3d7462c13359
nadil11 <nadilnidhal111@gmail.com>
2024-05-04 10:33:46 +0530

    last commit

--- pages/StripeNew.jsx
+++
@@ -1,3 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePromise = loadStripe('your_public_key_here');
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 
 import { Inter } from "next/font/google";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "@/firebase/firebase.config";
 import Stripe from "./Stripe";
 import Hero from "@/components/hero/Hero";
 import { useRouter } from "next/router";
@@ -8,11 +10,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
+  const GoogleSignIn = async ()=>{
+    const provider = new GoogleAuthProvider();
+
+    const authorization = auth;
+
+    const result = await signInWithPopup(authorization,provider);
+    console.log(result);
+  }
+
   const router = useRouter();
   const isBookingPage = router.pathname === "/booking";
 
   return (
     <div>
+      <button onClick={() => GoogleSignIn()}> Log in with google</button>
       <Hero/>
     </div>
   );
