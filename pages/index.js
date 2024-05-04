5cd421b3700a3c1ac1feae9775c73be0566f578d
nadil11 <nadilnidhal111@gmail.com>
2024-05-04 10:39:46 +0530

    last commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 
 import { Inter } from "next/font/google";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from "@/firebase/firebase.config";
-import Stripe from "./Stripe";
+// import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import { auth } from "@/firebase/firebase.config";
 import Hero from "@/components/hero/Hero";
 import { useRouter } from "next/router";
 
@@ -10,21 +9,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
-  const GoogleSignIn = async ()=>{
-    const provider = new GoogleAuthProvider();
+  // const GoogleSignIn = async ()=>{
+  //   const provider = new GoogleAuthProvider();
 
-    const authorization = auth;
+  //   const authorization = auth;
 
-    const result = await signInWithPopup(authorization,provider);
-    console.log(result);
-  }
+  //   const result = await signInWithPopup(authorization,provider);
+  //   console.log(result);
+  // }
 
   const router = useRouter();
   const isBookingPage = router.pathname === "/booking";
 
   return (
     <div>
-      <button onClick={() => GoogleSignIn()}> Log in with google</button>
+      {/* <button onClick={() => GoogleSignIn()}> Log in with google</button> */}
       <Hero/>
     </div>
   );
