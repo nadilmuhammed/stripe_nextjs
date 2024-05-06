30e63fc14f0d5369b4e1fd2d856e9a497ac96f46
nadil11 <nadilnidhal111@gmail.com>
2024-05-06 21:16:08 +0530

    last commit

--- pages/packages/landing/Card.jsx
+++ pages/packages/landing/Card.jsx
@@ -9,24 +9,24 @@ function Card({handleClose}) {
         Bank Card Details
       </div>
       <div className="bg-gray-100 p-4 rounded-lg mb-6">
-        <div className="flex justify-between items-center mb-4">
+        <div className="flex justify-between items-center mb-4 max-[768px]:flex-col">
           <span className="text-sm text-gray-600">
             Card Number
           </span>
           <input
             type='number'
-            className="text-lg font-semibold"
+            className="text-lg font-semibold text-gray-800 outline-none rounded"
             placeholder='**** **** **** 1234'
           />
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center max-[768px]:flex-col gap-3">
           <div className="flex flex-col">
             <span className="text-sm text-gray-600">
               Cardholder Name
             </span>
             <input
               type="text"
-              className="text-lg font-semibold"
+              className="text-lg font-semibold text-gray-800 outline-none rounded"
               placeholder="John Doe"
             />
           </div>
@@ -36,7 +36,7 @@ function Card({handleClose}) {
             </span>
             <input
               type="text"
-              className="text-lg font-semibold"
+              className="text-lg font-semibold text-gray-800 outline-none rounded"
               placeholder="12/24"
             />
           </div>
@@ -45,7 +45,7 @@ function Card({handleClose}) {
       <div className="text-sm text-gray-600 mb-2">CVV</div>
       <input
         type="text"
-        className="border border-gray-300 rounded-lg py-2 px-4 w-full mb-4"
+        className="border border-gray-300 rounded-lg py-2 px-4 w-full mb-4 text-gray-800 outline-none"
         placeholder="123"
       />
       <button className="bg-blue-500 text-white py-2 px-4 rounded-lg font-semibold">
